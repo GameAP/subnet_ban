ee542e663103cfe41b9d5ce0dcd66bfe8d202e74
ET-Nik <nikita.hldm@gmail.com>
2017-02-16 23:37:38 +0300

    Update version [GameAP 2.0]

--- web/application/modules/subnetban/module_info.php
+++ web/application/modules/subnetban/module_info.php
@@ -3,11 +3,11 @@
 $module_info['name'] 			= 'Бан подсетей';
 $module_info['cron_script'] 	= '';	
 $module_info['description'] 	= 'Бан подсетей для GoldSource серверов. Web основа для amxx плагина subnetban (http://aghl.ru/forum/viewtopic.php?f=19&t=282)';
-$module_info['version'] 		= '1.0';
+$module_info['version'] 		= '2.0';
 $module_info['show_in_menu'] 	= 1;
 $module_info['access'] 			= 'admin';
 $module_info['developer'] 		= 'ET-NiK';
 $module_info['site'] 			= 'http://hldm.org';
 $module_info['email'] 			= 'nikita@hldm.org';
-$module_info['copyright'] 		= '(c) 2014, ET-NiK (http://hldm.org)';
+$module_info['copyright'] 		= '(c) 2014-2016, ET-NiK (http://hldm.org)';
 $module_info['license'] 		= 'http://www.gameap.ru/license.html';
